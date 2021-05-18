e21eeb327af826b3bb8edccce3c0feff5053df22
OrlySedl <orlysedl@gmail.com>
2021-05-18 16:23:32 +0300

    Создан конструктор и свойства для класса NodeLinkedList.

--- Collections/LinkedList.cs
+++ Collections/LinkedList.cs
@@ -2,8 +2,16 @@
 
 namespace Orsel.Collections
 {
-    public class NodeLinkedList
+    public class NodeLinkedList<T>
     {
+        public T Data;
+        public NodeLinkedList<T> Next;
+
+        public NodeLinkedList(T data, NodeLinkedList<T> nextNode = null)
+        {
+            Data = data;
+            Next = nextNode;
+        }
     }
 
     public class LinkedList

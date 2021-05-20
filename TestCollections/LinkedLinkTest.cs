057db9834a9d96a34f55d48f144897b5c61423c4
OrlySedl <orlysedl@gmail.com>
2021-05-20 14:07:50 +0300

    Созданы тесты для Stack и LinkedList.

---
+++ TestCollections/LinkedLinkTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCollections
+{
+    [TestClass]
+    public class LinkedLinkTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

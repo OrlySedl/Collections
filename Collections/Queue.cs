4d66842c6757e726806b16b9db336909a8dac347
OrlySedl <orlysedl@gmail.com>
2021-05-29 20:26:01 +0300

    Добавлнеы поля array и defaultCapacity; свойства Capacity и Count. Создан конструктор класса.

--- Collections/Queue.cs
+++ Collections/Queue.cs
@@ -4,5 +4,24 @@ namespace Orsel.Collections
 {
     public class Queue
     {
+        private int[] array;
+        private const int defaultCapacity = 100;
+
+        public int Capacity
+        {
+            get;
+            set;
+        }
+
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        public Queue(int capacity = defaultCapacity)
+        {
+            array = new int[capacity];
+        }
     }
 }
